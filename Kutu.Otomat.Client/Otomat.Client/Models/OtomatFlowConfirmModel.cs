7240b2ba7886c8b978c2b7e99c68665ce7ad0c77
Serkan Ertan <s.ertan@lmu.de>
2020-07-20 11:42:57 +0200

    add Review to model

--- Kutu.Otomat.Client/Otomat.Client/Models/OtomatFlowConfirmModel.cs
+++ Kutu.Otomat.Client/Otomat.Client/Models/OtomatFlowConfirmModel.cs
@@ -3,5 +3,6 @@ namespace Otomat.Client.Models
     public class OtomatFlowConfirmModel
     {
         public string TcIdentityNumber { get; set; }
+        public int? Review { get; set; }
     }
 }
